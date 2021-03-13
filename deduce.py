7688d7b4f2cb3238ac6c0f1cc65ca2002955eb4b
PatrykWalach <patryk_walach_github@outlook.com>
2021-03-13 14:37:50 +0100

    feat: rewrite in python

---
+++ deduce.py
@@ -0,0 +1,56 @@
+import re
+
+text = open("baza-wiedzy.txt")
+
+initials = text.read().split("fakty")
+initialRules, initialFacts = (initials[0], initials[1])
+
+facts = set(s.strip() for s in initialFacts.split(','))
+rules = [tuple(s2.split(',') for s2 in re.sub(r'\d+\.', '', s.replace(" ", "")).split("->"))
+         for s in initialRules.split('\n') if s]
+
+
+def deduce(hypothesis):
+    if hypothesis in facts:
+        return True
+    changed = True
+    for requirements, results in rules:
+        if all(r in facts for r in results):
+            continue
+
+        if any(r not in facts for r in requirements):
+            continue
+
+        changed = True
+
+        for result in results:
+            facts.add(result)
+
+    return hypothesis in facts
+
+
+def deduceRight(hypothesis):
+    global facts
+    if hypothesis in facts:
+        return True
+
+    for requirements, results in rules:
+        if hypothesis not in results:
+            continue
+
+        if any(not deduceRight(r) for r in requirements):
+            continue
+
+        for result in results:
+            facts.add(result)
+
+        return True
+
+    return False
+
+
+for hypothesis in set(sum(list(sum(list(r), []) for r in rules), [])):
+    if deduceRight(hypothesis):
+        print(hypothesis, "jest spełnione")
+    else:
+        print(hypothesis, "nie jest spełnione")
