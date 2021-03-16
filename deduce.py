5da7b49a2a5d82417c281bbb8bd6bf8dae30968f
PatrykWalach <patryk_walach_github@outlook.com>
2021-03-16 14:06:38 +0100

    feat: deduce negation

--- deduce.py
+++ deduce.py
@@ -1,6 +1,6 @@
 import re
 
-text = open("baza-wiedzy.txt")
+text = open("baza-wiedzy-negacja.txt")
 
 initials = text.read().split("fakty")
 
@@ -8,40 +8,53 @@ text.close()
 
 initialRules, initialFacts = tuple(initials)
 
-facts = set(s.strip() for s in initialFacts.split(','))
 rules = [tuple(s2.split(',') for s2 in re.sub(r'\d+\.', '', s.replace("~~", "").replace(" ", "")).split("->"))
          for s in initialRules.split('\n') if s]
 
 
-def deduce(hypothesis):
-    if hypothesis in facts:
+def check(hypothesis, facts):
+    return (hypothesis.replace('~', '') in facts) ^ hypothesis.startswith('~')
+
+
+def deduce(hypothesis, facts):
+    """Funkcja wykonuje wnioskowanie w przód."""
+    if check(hypothesis, facts):
         return True
+
     changed = True
-    for requirements, results in rules:
-        if all(r in facts for r in results):
-            continue
+    while changed:
+        changed = False
 
-        if any(r not in facts for r in requirements):
-            continue
+        for (requirements, results), i in zip(rules, range(len(rules))):
 
-        changed = True
+            if not all(check(requirement, facts) for requirement in requirements):
+                if not all(any(all(check(requirement, facts) for requirement in requirements) for requirements, results in rules if result in results) for result in results):
+                    for result in results:
+                        if result in facts:
+                            facts.remove(result)
+                            changed = True
+                continue
 
-        for result in results:
-            facts.add(result)
+            if all(r in facts for r in results):
+                continue
+
+            changed = True
+            for result in results:
+                facts.add(result)
 
     return hypothesis in facts
 
 
-def deduceRight(hypothesis):
-    global facts
-    if hypothesis in facts:
+def deduceRight(hypothesis, facts):
+    """Funkcja wykonuje wnioskowanie w tył."""
+    if check(hypothesis, facts):
         return True
 
     for requirements, results in rules:
         if hypothesis not in results:
             continue
 
-        if any(not deduceRight(r.replace('~', '')) ^ r.startswith('~') for r in requirements):
+        if any(not deduceRight(r.replace('~', ''), facts) ^ r.startswith('~') for r in requirements):
             continue
 
         for result in results:
@@ -53,7 +66,7 @@ def deduceRight(hypothesis):
 
 
 for hypothesis in sorted(set(r.replace('~', '') for r in sum(list(sum(list(r), []) for r in rules), []))):
-    if deduceRight(hypothesis):
+    if deduce(hypothesis, set(s.strip() for s in initialFacts.split(','))):
         print(hypothesis, "jest spełnione")
     else:
         print(hypothesis, "nie jest spełnione")
