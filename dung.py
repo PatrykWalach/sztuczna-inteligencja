b579c6e24890c681f990043d462a88a946c65e31
PatrykWalach <patryk_walach_github@outlook.com>
2021-03-23 21:37:31 +0100

    refactor: decouple

--- dung.py
+++ dung.py
@@ -1,55 +1,36 @@
 import re
-
-text = open("dung.txt")
-
-initials = re.split(r"Attacks.*?\n", text.read(), flags=re.IGNORECASE)
-text.close()
+from queue import PriorityQueue
 
 
-initialArgs, initialAttacks = tuple(initials)
-
-vertices = re.compile(r'Args.*?\n', re.IGNORECASE).sub('',
-                                                       initialArgs).strip().split(',')
-
-edges = [[False]*len(vertices) for v in vertices]
-
-for attack in initialAttacks.split('),('):
-    a, d = tuple(attack.replace(")", '').replace('(', '').split(','))
-    edges[vertices.index(d)][vertices.index(a)] = True
+def outLen(vertices: list, edges: list, fullfilled: list, d: int):
+    return len([a for a, i in zip(edges[d], range(len(vertices))) if a is True and fullfilled[i] is not True])
 
 
-def edgesLen(d):
-    return len(
-        [e for e in edges[vertices.index(d)] if e is True]
-        # list(filter(lambda e: e is True, edges[vertices.index(d)]))
-    )
-
-
-fullfilled = [None]*len(vertices)
-
-
-def dung(fullfilled):
+def dung(vertices: list, edges: list, fullfilled: list):
     visited = [False]*len(vertices)
-    queue = sorted(vertices, key=edgesLen)
+    queue = PriorityQueue()
+    for e in sorted([(outLen(vertices, edges, fullfilled, d), d)
+                     for d in range(len(vertices))]):
+        queue.put(e)
 
-    while(len(queue)):
-        d = queue.pop(0)
+    while(not queue.empty()):
+        w, d = queue.get()
 
-        if visited[vertices.index(d)]:
-            continue
-        visited[vertices.index(d)] = True
-
-        for e in sorted([vertices[a]
-                         for a in range(len(vertices)) if edges[a][vertices.index(d)]], key=edgesLen, reverse=True):
-            queue.insert(0, e)
-
-        if fullfilled[vertices.index(d)] is not None:
+        if visited[d]:
             continue
 
-        if any(fullfilled[a] for a in range(len(vertices)) if edges[vertices.index(d)][a]):
-            fullfilled[vertices.index(d)] = False
-        elif all(fullfilled[a] is False for a in range(len(vertices)) if edges[vertices.index(d)][a]):
-            fullfilled[vertices.index(d)] = True
+        visited[d] = True
+
+        # if fullfilled[d] is not None:
+        #     continue
+
+        if any(fullfilled[a] for a in range(len(vertices)) if edges[d][a]):
+            fullfilled[d] = False
+        elif all(fullfilled[a] is False for a in range(len(vertices)) if edges[d][a]):
+            fullfilled[d] = True
+
+        for e in [a for a in range(len(vertices)) if edges[a][d]]:
+            queue.put((outLen(vertices, edges, fullfilled, e), e))
 
 
 class c:
@@ -59,52 +40,71 @@ class c:
     END = '\033[0;0m'
 
 
-def printVertices(fullfilled):
+def printVertices(vertices: list,  fullfilled: list,):
     for v in range(len(vertices)):
         if fullfilled[v] is True:
             print(' · '+vertices[v] + f' is {c.GREEN}In{c.END}')
         elif fullfilled[v] is False:
             print(' · '+vertices[v] + f' is {c.ORANGE}Out{c.END}')
         else:
-            print(' · '+vertices[v] + f' is {c.GRAY}Nither{c.END}')
+            print(' · '+vertices[v] + f' is {c.GRAY}Unknown{c.END}')
 
 
-def resolveNither(u):
-    fullfilledCopy = fullfilled.copy()
-    fullfilledCopy[u] = True
-    dung(fullfilledCopy)
-    return fullfilledCopy
+def resolveNither(vertices: list, edges: list, fullfilled: list, u: int):
+    fullfilled[u] = True
+    dung(vertices, edges,  fullfilled)
+    return fullfilled
 
 
-def trueLen(l):
-    return len([e for e in l if l is True])
+def trueLen(l: list):
+    return len([e for e in l if e is True])
 
 
-def allMax(l, **kargs):
-    return [n for n in resolvedStates if kargs.get('key', lambda x:x)(resolvedStates) is max(map(kargs.get('key', lambda x:x), resolvedStates))]
+def allMax(l: list, **kargs):
+    return [n for n in l if n is max(l, key=kargs.get('key'))]
 
 
-def allMin(l, **kargs):
-    return [n for n in resolvedStates if kargs.get('key', lambda x:x)(resolvedStates) is min(map(kargs.get('key', lambda x:x), resolvedStates))]
+def allMin(l: list, **kargs):
+    return [n for n in l if n is min(l, key=kargs.get('key'))]
 
 
-dung(fullfilled)
-print('Stable semantics:')
-printVertices(fullfilled)
+def main():
+    text = open("dung.txt")
+
+    initials = re.split(r"Attacks.*?\n", text.read(), flags=re.IGNORECASE)
+    text.close()
+
+    initialArgs, initialAttacks = tuple(initials)
+
+    vertices = re.compile(r'Args.*?\n', re.IGNORECASE).sub('',
+                                                           initialArgs).strip().split(',')
+
+    edges = [[False]*len(vertices) for v in vertices]
+
+    for attack in initialAttacks.split('),('):
+        a, d = tuple(attack.replace(")", '').replace('(', '').split(','))
+        edges[vertices.index(d)][vertices.index(a)] = True
+
+    fullfilled = [None]*len(vertices)
+
+    dung(vertices, edges, fullfilled)
+    print('Stable semantics:')
+    printVertices(vertices, fullfilled)
+
+    resolvedStates = [resolveNither(vertices, edges, fullfilled.copy(), v) for v in range(
+        len(vertices)) if fullfilled[v] is None]
+
+    print('Grounded semantics:')
+    for states, i in zip(allMin(resolvedStates, key=trueLen), range(len(resolvedStates))):
+        if i:
+            print('or:')
+        printVertices(vertices, states)
+
+    print('Prefferred semantics:')
+    for states, i in zip(allMax(resolvedStates, key=trueLen), range(len(resolvedStates))):
+        if i:
+            print('or:')
+        printVertices(vertices, states)
 
 
-resolvedStates = [resolveNither(v) for v in range(
-    len(vertices)) if fullfilled[v] is None]
-
-
-print('Grounded semantics:')
-for states, i in zip(allMin(resolvedStates, key=trueLen), range(len(resolvedStates))):
-    if i:
-        print('or:')
-    printVertices(states)
-
-print('Prefferred semantics:')
-for states, i in zip(allMax(resolvedStates, key=trueLen), range(len(resolvedStates))):
-    if i:
-        print('or:')
-    printVertices(states)
+main()
