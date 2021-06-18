15c52fedb428ed80d9e07d19a43a755279844187
PatrykWalach <patryk_walach_github@outlook.com>
2021-06-18 21:14:39 +0200

    feat: use train-amazon as df

--- lstm.py
+++ lstm.py
@@ -21,12 +21,18 @@ def wynik(y_test, predictions):
     print(metrics.confusion_matrix(y_test, predictions))
 
 
-df = pd.read_csv('smsspamcollection.tsv', sep='\t')
-X = df['message']
+word2vec_model = gensim.models.KeyedVectors.load_word2vec_format(
+    'enwiki_20180420_100d.txt', binary=False)
+
+print('Model loaded')
+# %%
+
+df = pd.read_csv('train-amazon.tsv', sep='\t')
+X = df['review']
 z = df['label']
 y = []
 for i in z:
-    if i == 'spam':
+    if i == 'pos':
         k = 0
     else:
         k = 1
@@ -41,18 +47,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 X_train, X_val, y_train, y_val = train_test_split(
     X_train, y_train, test_size=0.2, random_state=42)
 
-word2vec_model = gensim.models.KeyedVectors.load_word2vec_format(
-    'enwiki_20180420_100d.txt', binary=False)
 
 print(dir(word2vec_model))
-# %%
 embedding_matrix = word2vec_model.vectors
 
-print(embedding_matrix)
-print(X)
 
 top_words = embedding_matrix.shape[0]
-mxlen = 20
+mxlen = 100
 
 
 tokenizer = Tokenizer(num_words=top_words)
@@ -65,9 +66,9 @@ word_index = tokenizer.word_index
 print('Found %s unique tokens.' % len(word_index))
 
 
-X_train = sequence.pad_sequences(sequences_train, maxlen=mxlen)
-X_test = sequence.pad_sequences(sequences_test, maxlen=mxlen)
-X_val = sequence.pad_sequences(sequences_val, maxlen=mxlen)
+X_train_padded = sequence.pad_sequences(sequences_train, maxlen=mxlen)
+X_test_padded = sequence.pad_sequences(sequences_test, maxlen=mxlen)
+X_val_padded = sequence.pad_sequences(sequences_val, maxlen=mxlen)
 
 batch_size = 32
 nb_epoch = 100
@@ -79,8 +80,7 @@ embedding_layer = Embedding(embedding_matrix.shape[0],
 
 model = Sequential()
 model.add(embedding_layer)
-
-model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+model.add(LSTM(128, dropout=0.3, recurrent_dropout=0.2))
 model.add(Dropout(0.2))
 model.add(Dense(20, input_dim=129, activation='relu', use_bias=True))
 model.add(Dense(2, use_bias=True))
@@ -90,15 +90,15 @@ model.summary()
 
 model.compile(optimizer='adam', loss='categorical_crossentropy',
               metrics=['accuracy'])
-rnn = model.fit(X_train, y_train, epochs=nb_epoch, batch_size=batch_size,
-                shuffle=True, validation_data=(X_val, y_val))
+rnn = model.fit(X_train_padded, y_train, epochs=nb_epoch, batch_size=batch_size,
+                shuffle=True, validation_data=(X_val_padded, y_val))
 
 # model.save('model-eng-embed-nn.h5')
 #dump(tokenizer, open('tokeny-embedd-en', 'wb'))
 
 # testy
 print('testowe')
-predykcje = model.predict(X_test)
+predykcje = model.predict(X_test_padded)
 
 predictions = predykcje.round()
 
